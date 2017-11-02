42ebafa90236032cac8a57c54b0322e3139156f7
David Mandelberg <david@mandelberg.org>
2017-11-01 21:00:27 -0400

    Play around with trigonometric functions and moiré-like patterns.
    I'm not sure yet if I'm going to do anything with this, but it's a slightly
    interesting effect.

---
+++ trig-moire/trig-moire.cc
@@ -0,0 +1,67 @@
+#include <libnoise/module/perlin.h>
+
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+
+#define PI 3.141592653589793
+
+#define ASPECT_RATIO ((float)(WIDTH)/(float)(HEIGHT))
+
+int main() {
+  ::noise::module::Perlin perlin;
+  perlin.SetOctaveCount(1);
+  perlin.SetFrequency(1.0);
+  perlin.SetNoiseQuality(::noise::QUALITY_BEST);
+  perlin.SetSeed(SEED);
+
+  for (int frame_number = 0; frame_number < FRAME_COUNT; ++frame_number) {
+    double real_t = (double)frame_number / FRAMERATE;
+    real_t = 1.0 + 0.05 * real_t;
+
+    double unit_x[FACTOR_COUNT];
+    double unit_y[FACTOR_COUNT];
+    double period[FACTOR_COUNT];
+    for (int factor_number = 0; factor_number < FACTOR_COUNT; ++factor_number) {
+      double theta = PI * perlin.GetValue(factor_number, 0.0, real_t);
+      unit_x[factor_number] = cos(theta);
+      unit_y[factor_number] = sin(theta);
+
+      period[factor_number] = 0.5 * perlin.GetValue(factor_number, 1.0, real_t) + 0.5;
+      period[factor_number] = ::std::max(0.01, period[factor_number]);
+
+      //fprintf(stderr, "%06d[%02d]: (%lf, %lf) / %lf\n", frame_number, factor_number, unit_x[factor_number], unit_y[factor_number], period[factor_number]);
+    }
+
+    for (int y = 0; y < HEIGHT; ++y) {
+      float real_y = (2.0f * y) / HEIGHT - 1.0f;
+      if (ASPECT_RATIO < 1.0f) {
+        real_y /= ASPECT_RATIO;
+      }
+
+      for (int x = 0; x < WIDTH; ++x) {
+        float real_x = (2.0f * x) / HEIGHT - 1.0f;
+        if (ASPECT_RATIO > 1.0f) {
+          real_x *= ASPECT_RATIO;
+        }
+
+        float luma = 1.0f;
+        for (int factor_number = 0; factor_number < FACTOR_COUNT; ++factor_number) {
+          double d = real_x * unit_x[factor_number] + real_y * unit_y[factor_number];
+          luma *= 0.5 * sin(d / period[factor_number]) + 0.5;
+        }
+
+        float pixel = luma * 256.0f;
+        if (pixel < 0.0f) {
+          putchar(0);
+        } else if (pixel > 255.0f) {
+          putchar(255);
+        } else {
+          putchar((int)pixel);
+        }
+      }
+    }
+  }
+
+  return 0;
+}
