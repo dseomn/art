c18cac17be568ee562d82211990de4f4281de6c6
David Mandelberg <david@mandelberg.org>
2017-11-27 18:37:34 -0500

    Add .mp4 output.

--- overlapping-sin-waves/music-video.sh
+++ overlapping-sin-waves/music-video.sh
@@ -18,7 +18,8 @@ for f in final/track-??-*.flac; do
         [1:a] showspectrum=size=1193x1363:slide=scroll:color=channel:mode=combined, fps=fps=60, scale=size=2386x1363 [spectrum];
         [1:a] showwaves=size=1600x1600:mode=p2p:rate=60:split_channels=1, dilation, dilation, dilation, dilation, scale=size=1024x1024 [waves];
         [0:v][spectrum] overlay=x=1408:y=742:shortest=1 [tmpa];
-        [tmpa][waves] overlay=x=43:y=1082 [video]
+        [tmpa][waves] overlay=x=43:y=1082 [video];
+        [video] split [video0][video1]
         ' \
     -c:a libvorbis \
     -q:a 10 \
@@ -28,6 +29,14 @@ for f in final/track-??-*.flac; do
     -g 240 \
     -tile-columns 4 \
     -threads 6 \
-    -map '[video]' -map 1:a "video/${tag}.webm"
+    -map '[video0]' -map 1:a "video/${tag}.webm" \
+    -c:a libmp3lame \
+    -q:a 0 \
+    -c:v libx264 \
+    -preset slower \
+    -crf 17 \
+    -g 240 \
+    -map '[video1]' -map 1:a "video/${tag}.mp4"
+  try_v ln -s "../video/${tag}.mp4" final/
   try_v ln -s "../video/${tag}.webm" final/
 done
